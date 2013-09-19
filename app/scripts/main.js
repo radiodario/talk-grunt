7d55dc504510c81d5ce37dc7b52393c8eb686cfe
Dario Villanueva <dario.villanueva@gmail.com>
2013-09-19 16:05:35 +0100

    much updated

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,19 +2,27 @@ require.config({
     paths: {
         jquery: '../bower_components/jquery/jquery',
         deckjs: '../bower_components/deck.js/core/deck.core',
-        deckjsHash: '../bower_components/deck.js/extensions/hash/deck.hash'
+        deckjsHash: '../bower_components/deck.js/extensions/hash/deck.hash',
+        deckjsCodemirror : '../bower_components/deck.js-codemirror/deck.codemirror',
+        codemirror: '../bower_components/deck.js-codemirror/codemirror',
     },
     shim: {
+        codemirror: {
+            exports: 'codemirror'
+        },
         deckjs: {
             deps: ['jquery']
         },
         deckjsHash: {
             deps: ['deckjs']
+        },
+        deckjsCodemirror: {
+            deps: ['codemirror', 'deckjs']
         }
     }
 });
 
-require(['app', 'jquery', 'deckjsHash', 'deckjs'], function (app, $) {
+require(['app', 'jquery', 'deckjsHash', 'deckjsCodemirror', 'deckjs'], function (app, $) {
     'use strict';
     // use app here
     $.deck('.slide')
